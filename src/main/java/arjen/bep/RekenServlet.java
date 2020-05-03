e086e69c90e78396cbbf733b999dd4c3b8be2a10
arjennorbart <57788003+arjennorbart@users.noreply.github.com>
2020-05-03 11:09:37 +0200

    oplossing voor afronden geimplementeerd

--- src/main/java/arjen/bep/RekenServlet.java
+++ src/main/java/arjen/bep/RekenServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.Enumeration;
 
 import static java.lang.Double.parseDouble;
@@ -25,37 +26,46 @@ public class RekenServlet extends HttpServlet {
                 stringBuilder.append(req.getParameter(parameter.nextElement()) + " ");
             }
         }
-        double num1 = parseDouble(stringBuilder.toString().split(" ")[0]);
-        double num2 = parseDouble(stringBuilder.toString().split(" ")[1]);
+        String num1 = "";
+        String num2 = "";
+        double number1 = parseDouble(stringBuilder.toString().split(" ")[0]);
+        if (number1 % 1 == 0)
+             num1 = String.format("%.0f", number1);
+        else num1 = String.format("%s", number1);
+
+        double number2 = parseDouble(stringBuilder.toString().split(" ")[1]);
+        if (number2 % 1 == 0)
+            num2 = String.format("%.0f", number2);
+        else num2 = String.format("%s", number2);
+
         String operator = stringBuilder.toString().split(" ")[2];
         String afronden = stringBuilder.substring(Math.max(stringBuilder.length() - 3, 0));
 
         if (operator.equals("-")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(num1 - num2)));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, num1 - num2));
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(number1 - number2)));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, number1 - number2));
         } else if (operator.equals("+")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(num1 + num2)));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, num1 + num2));
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(number1 + number2)));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, number1 + number2));
         } else if (operator.equals("*")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(num1 * num2)));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, num1 * num2));
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(number1 * number2)));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, number1 * number2));
         } else if (operator.equals("/")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(num1 / num2)));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, num1 / num2));
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(number1 / number2)));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, number1 / number2));
         } else if (operator.equals("%")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(num1 % num2)));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, num1 % num2));
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(number1 % number2)));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, number1 % number2));
         } else if (operator.equals("^")) {
             if (afronden.contains("ja"))
-                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(Math.pow(num1, num2))));
-            else out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.pow(num1, num2)));
-        }
-        else out.write("iets mis gegaan");
+                out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.round(Math.pow(number1, number2))));
+            else out.write(String.format("%s %s %s = %s", num1, operator, num2, Math.pow(number1, number2)));
+        } else out.write("iets mis gegaan");
     }
 }
 
