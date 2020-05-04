e2438fd29c8babd29bd2d2d4a008e807c7978357
arjennorbart <57788003+arjennorbart@users.noreply.github.com>
2020-05-04 14:35:05 +0200

    practica 4.2 af

---
+++ src/main/java/arjen/servlets/RekeningListener.java
@@ -0,0 +1,38 @@
+package arjen.servlets;
+
+import arjen.models.Bank;
+import arjen.models.Rekening;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class RekeningListener implements ServletContextListener {
+
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Applicatie is aan het opstarten!");
+
+        Rekening rekening1 = new Rekening("NL88INGB0009635096", "Arjen Norbart", 5);
+        Rekening rekening2 = new Rekening("NL60INGB0001535096", "Kees", 20.22);
+        Rekening rekening3 = new Rekening("NL70INGB0001212121", "Conjones", 3000.45);
+        Rekening rekening4 = new Rekening("NL00INGB0000000000", "Mr. Zero", 500.82);
+        Rekening rekening5 = new Rekening("NL69INGB6969696969", "Kama Sutra", 69);
+
+        Bank.getBank().addRekening(rekening1);
+        Bank.getBank().addRekening(rekening2);
+        Bank.getBank().addRekening(rekening3);
+        Bank.getBank().addRekening(rekening4);
+        Bank.getBank().addRekening(rekening5);
+
+        rekening1.stort(245); // zou moeten werken. Bedrag zou nu 250 moeten zijn
+        rekening3.boekOver(250, rekening1); // bedrag rekening 1 zou nu 500 moeten zijn
+        rekening5.boekOver(100, rekening2); // zou moeten falen omdat saldo te laag is. Fixen in practica 4.3 (tests)
+
+        System.out.println("rekeningen aangemaakt + transacties gedaan");
+
+    }
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("applicatie gaat afsluiten");
+    }
+}
